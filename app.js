995154a1ebca85721314a7ca15c960248dcd136d
Paruyr91 <paruyr91@gmail.com>
2021-01-13 15:44:16 +0400

    firstf

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ const app =express()
 const bodyParser=require('body-parser')
 const server=require('http').Server(app)
 require('dotenv').config()
-const PORT=process.env.PORT??3000
+const PORT=process.env.PORT??80
 
 const router=require('./server/router')
 
 app.use(router)
 
 
-server.listen(PORT, ()=>{
-console.log('Server started...')
+server.listen(80, ()=>{
+console.log(`server started at${PORT}`)
 }) 
