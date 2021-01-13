159521fce796348653f6af103a0fdcffa2a1af0c
Paruyr91 <paruyr91@gmail.com>
2021-01-13 15:18:14 +0400

    firstf

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const express = require('express')
 const app =express()
 const bodyParser=require('body-parser')
 const server=require('http').Server(app)
+require('dotenv').config()
 const PORT=process.env.PORT??3000
+
 const router=require('./server/router')
 
 app.use(router)
+
 
 server.listen(PORT, ()=>{
 console.log('Server started...')
