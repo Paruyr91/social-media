bcf58856cc4c6b82a13908d1441418a5323dae59
Paruyr91 <paruyr91@gmail.com>
2021-01-13 14:29:10 +0400

    firsttf

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app =express()
 const bodyParser=require('body-parser')
 const server=require('http').Server(app)
-const PORT=process.env.PORT
+const PORT=process.env.PORT??3000
 
 
-server.listen(PORT)
+server.listen(PORT) 
