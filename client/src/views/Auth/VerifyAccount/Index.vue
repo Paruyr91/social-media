24cf2eb791823620fb8e971b665491c9cb556451
Artur <arturdavtyanars@gmail.com>
2021-01-22 18:54:04 +0400

    add verify account

---
+++ client/src/views/Auth/VerifyAccount/Index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="main-container w-full h-screen relative">
+    verify account
+  </div>
+</template>
+
+<script>
+// import screenSize from '@/mixins/screenSize'
+
+export default {
+  // mixins: [screenSize]
+}
+</script>
+
+<style lang="scss" scoped>
+  @import './scss/';
+</style>
