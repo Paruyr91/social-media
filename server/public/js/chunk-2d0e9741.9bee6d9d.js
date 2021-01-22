945a1c72b67385930be05fc59d390513933aa080
Paruyr91 <paruyr91@gmail.com>
2021-01-22 17:24:18 +0400

    client

--- server/public/js/chunk-2d0e9741.9bee6d9d.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0e9741"],{"8e31":function(e,n,t){"use strict";t.r(n);var u=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[t("h1",[e._v("Main")]),t("router-view")],1)},r=[],c=t("2877"),i={},l=Object(c["a"])(i,u,r,!1,null,null,null);n["default"]=l.exports}}]);
-//# sourceMappingURL=chunk-2d0e9741.9bee6d9d.js.map
