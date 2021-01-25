e830867ef0ada2edc24fcb58d23f85c3ec26c3f8
Paruyr91 <paruyr91@gmail.com>
2021-01-25 16:15:22 +0400

    client

--- server/models/user.js
+++ server/models/user.js
@@ -1,10 +1,10 @@
 
 const { Sequelize, Op, Model, DataTypes} = require('sequelize');
 const sequelize = require('./index')
-  const bcrypt=require('bcrypt');
-  const hashNumber=10
+const bcrypt=require('bcrypt');
+const hashNumber=10
 
-  const User = sequelize.define("users", {
+const User = sequelize.define("users", {
     email: {type: DataTypes.STRING,
                allowNull:false,
                validate: {
@@ -53,31 +53,23 @@ const sequelize = require('./index')
     activated:{type:Sequelize.BOOLEAN, 
                allowNull: false, 
                defaultValue:false
-               }
-
-             
-  });
-
-
- 
-  User.beforeCreate(async (user) => {
-    user.password= await bcrypt.hashSync(user.password,hashNumber);
+               }      
   });
  
+User.beforeCreate(async (user) => {
+  user.password= await bcrypt.hashSync(user.password,hashNumber);
+});
  
- 
-  User.beforeUpdate(async (user) => {
-    user.password= await bcrypt.hashSync(user.password,hashNumber);
-    
-  });
+User.beforeUpdate(async (user) => {
+  if(user.dataValues.password!==user._previousDataValues.password){
+      user.password= await bcrypt.hashSync(user.password,hashNumber);
+  }
+});
    
-  
 
 //  User.sync({force:true}) 
  
- 
- 
-  module.exports=User
+module.exports=User
  
  
  
